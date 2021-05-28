1dbf8a7c834b19d70ff17220a8dbd49c101a6c3d
abhijith7025 <abhijithbabu07@gmail.com>
2021-05-28 14:48:55 +0530

    another db edit

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'quickstart'),
-            'username'  => env('DB_USERNAME', 'quickstart'),
-            'password'  => env('DB_PASSWORD', 'quickstart'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
